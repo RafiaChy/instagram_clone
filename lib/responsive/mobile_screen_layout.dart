f029766f6ab056d00bfaab41b5f49cc537b88b7b
RafiaChy <rafiachowdhury24@gmail.com>
2022-05-30 22:08:54 +0600

    used PageController

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -1,7 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/providers/user_provider.dart';
+import 'package:instagram_clone/utils/colors.dart';
 import 'package:provider/provider.dart';
 import 'package:instagram_clone/models/user.dart' as model;
 
@@ -13,6 +15,8 @@ class MobileScreenLayout extends StatefulWidget {
 }
 
 class _MobileScreenLayoutState extends State<MobileScreenLayout> {
+
+ 
 
   // String username = '';
   // @override
@@ -34,12 +38,50 @@ class _MobileScreenLayoutState extends State<MobileScreenLayout> {
   //   username = (snap.data() as Map<String, dynamic>)['username'];
   //   });
   // }
+  late PageController pageController;
+  @override
+  void initState() {
+    pageController = PageController();
+    super.initState();
+  }
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+   int _page = 0;
+
+  void navigationTap(int page){
+    pageController.jumpTo(page.toDouble());
+  }
 
   @override
   Widget build(BuildContext context) {
-    model.User user = Provider.of<UserProvider>(context).getUser;
+   // model.User user = Provider.of<UserProvider>(context).getUser;
+  
     return Scaffold(
-      body: Center(child: Text(user.username),),
+      body: PageView(
+        children: const [
+           Center(child: Text('feed'),),
+           Center(child: Text('search'),),
+           Center(child: Text('add'),),
+           Center(child: Text('notifications'),),
+           Center(child: Text('profile'),),
+        ],
+        controller: pageController,
+        onPageChanged: null,
+      ),
+      bottomNavigationBar: CupertinoTabBar(
+        backgroundColor: mobileBackgroundColor,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: '', backgroundColor: _page == 0? primaryColor : secondaryColor, ),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: '', backgroundColor:  _page == 1? primaryColor : secondaryColor, ),
+          BottomNavigationBarItem(icon: Icon(Icons.add_circle), label: '', backgroundColor:  _page == 2? primaryColor : secondaryColor, ),
+          BottomNavigationBarItem(icon: Icon(Icons.favorite), label: '', backgroundColor:  _page == 3? primaryColor : secondaryColor, ),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: '', backgroundColor:  _page == 4? primaryColor : secondaryColor, ),
+        ],
+      onTap: navigationTap
+      ),
     );
   }
 }
