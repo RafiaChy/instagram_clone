a953668f1743d023c20ac209dc94a58f8f20fdca
RafiaChy <rafiachowdhury24@gmail.com>
2022-06-02 20:06:10 +0600

    Modifying screens

--- lib/screens/add_post_screen.dart
+++ lib/screens/add_post_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:instagram_clone/utils/colors.dart';
 
 class AddPostScreen extends StatefulWidget {
   const AddPostScreen({Key? key}) : super(key: key);
@@ -13,8 +14,16 @@ class AddPostScreen extends StatefulWidget {
 class _AddPostScreenState extends State<AddPostScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: IconButton(onPressed: (){}, icon: Icon(Icons.upload))
+    // return Center(
+    //   child: IconButton(onPressed: (){}, icon: Icon(Icons.upload))
+    // );
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
+        leading: IconButton(icon: const Icon(Icons.arrow_back), onPressed: (){},),
+        title: const Text('Post To'),
+      ),
+      
     );
   }
 }
