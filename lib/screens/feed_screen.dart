09095ab5feecb5153e9770f435eeea714575b38f
RafiaChy <rafiachowdhury24@gmail.com>
2022-06-10 22:06:30 +0600

    added circle avatar and user name for the post card

--- lib/screens/feed_screen.dart
+++ lib/screens/feed_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:instagram_clone/utils/colors.dart';
+import 'package:instagram_clone/widgets/post_card.dart';
 
 class FeedScreen extends StatelessWidget {
   const FeedScreen({Key? key}) : super(key: key);
@@ -16,7 +17,7 @@ class FeedScreen extends StatelessWidget {
           IconButton(onPressed: (){}, icon: Icon(Icons.message_outlined))
         ],
       ),
-      
+      body: const PostCard(),
     );
   }
 }
---
+++ lib/widgets/post_card.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/utils/colors.dart';
+
+class PostCard extends StatelessWidget {
+  const PostCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: mobileBackgroundColor,
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(
+              vertical: 4,
+              horizontal: 16,
+            ).copyWith(right: 0),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: 16,
+                  backgroundImage: NetworkImage('https://images.unsplash.com/photo-1654796572930-2cfe46670b2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=465&q=80'),
+
+                ),
+                Expanded(child: Padding(padding: const EdgeInsets.only(left: 8),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('username', style: TextStyle(fontWeight: FontWeight.bold),)
+                  ],
+                )
+                ,))
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
