a6a0a67a27642a434487ac7f8c06f098625bdc3f
RafiaChy <rafiachowdhury24@gmail.com>
2022-06-10 22:20:28 +0600

    added delete button

--- lib/widgets/post_card.dart
+++ lib/widgets/post_card.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/utils/colors.dart';
 
@@ -18,7 +20,7 @@ class PostCard extends StatelessWidget {
             ).copyWith(right: 0),
             child: Row(
               children: [
-                CircleAvatar(
+                const CircleAvatar(
                   radius: 16,
                   backgroundImage: NetworkImage('https://images.unsplash.com/photo-1654796572930-2cfe46670b2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=465&q=80'),
 
@@ -33,7 +35,25 @@ class PostCard extends StatelessWidget {
                 )
                 ,),
                 ),
-              IconButton(onPressed: (){}, icon: Icon(Icons.more_vert))
+              IconButton(onPressed: (){
+                showDialog(context: context, builder:(context)=>
+                Dialog(
+                  child: ListView(
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    shrinkWrap: true,
+                    children: [
+                      'Delete',
+                    ].map((e) => InkWell(
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+                        child: Text(e),
+                      ),
+                      onTap: (){},
+                    ),
+                    ).toList()
+                  ),
+                ));
+              }, icon: Icon(Icons.more_vert))
               ],
             ),
           )
