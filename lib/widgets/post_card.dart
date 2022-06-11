1d0f447d1eef1fafa61c92ec2ad92ef7bef9dff0
RafiaChy <rafiachowdhury24@gmail.com>
2022-06-11 15:22:00 +0600

    completed the post card

--- lib/widgets/post_card.dart
+++ lib/widgets/post_card.dart
@@ -66,7 +66,6 @@ class PostCard extends StatelessWidget {
             child: Image.network('https://images.unsplash.com/photo-1654847129539-3ef544f24036?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
             fit: BoxFit.cover,),
           ),
-
           //LIKE COMMENT SHARE SECTION
           Row(
             children: [
@@ -79,12 +78,72 @@ class PostCard extends StatelessWidget {
                   alignment: Alignment.bottomRight,
                   child: IconButton(onPressed: (){}, icon: Icon(Icons.bookmark_border_outlined),),
                   ),
-
                ),
             ],
-          )
-        ],
+          ),
+          //DESCRIPTION NUMBER OF COMMENTS
+          Container(
+            padding: const EdgeInsets.symmetric(
+            horizontal: 16,
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                DefaultTextStyle(
+                  style: Theme.of(context).textTheme.subtitle2!.copyWith(fontWeight: FontWeight.w800),
+                child: Text('1,231 likes', style: Theme.of(context).textTheme.bodyText2,),
+                ),
+                Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.only(
+                    top: 8,
+                  ),
+                  child: RichText(
+                    text: TextSpan(
+                      style: const TextStyle(
+                        color: primaryColor
+                      ) ,
+                      children: [
+                        TextSpan(
+                          text: 'Username',
+                          style: const TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                          TextSpan(
+                          text: ' Hey this is a description to be replaced',
+                          
+                        ),
+                      ]
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: (){},
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 4,
+                    ),
+                    child: Text('View all 200 comments', style: const TextStyle(
+                      fontSize: 16,
+                      color: secondaryColor,
+                    ),) ,
+                  ),
+                ),
 
+                Container(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 4,
+                    ),
+                    child: Text('6/11/2022', style: const TextStyle(
+                      fontSize: 16,
+                      color: secondaryColor,
+                    ),) ,
+                  ),
+              ],
+
+            ),
+          ),
+        ],
       ),
     );
   }
